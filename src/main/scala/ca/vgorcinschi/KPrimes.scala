dc758c36ebc3d9d6205872ed08985d2845b244c5
Vasile Gorcinschi <vasigorc@gmail.com>
2018-08-25 10:54:50 -0400

    KPrimes completed

--- src/main/scala/ca/vgorcinschi/KPrimes.scala
+++ src/main/scala/ca/vgorcinschi/KPrimes.scala
@@ -1,15 +1,28 @@
 package ca.vgorcinschi
 
 object KPrimes {
-  def countKprimes(k:  Int, start: Long, end: Long): String = {
-    countHelper(k, start, end).mkString(", ")
+
+  implicit class StreamOps[T](stream: Stream[T]){
+
+    def takeUntil(test: T => Boolean): Stream[T]={
+      stream.span(test) match {
+        case (before, after) => before #::: after.take(1)
+      }
+    }
   }
 
-  def countHelper(k: Int, start: Long, end: Long): List[Int] = {
-    def loop(n: Int): Stream[Int] =
-      if(isKPrime(n, k)) n #:: loop(n + 1)
-      else loop(n + 1)
-    loop(2).dropWhile(_.toLong < start).takeWhile(_.toLong < end).toList
+  def countKprimes(k:  Int, start: Long, end: Long): String = {
+    kPrimes(k, start, end).mkString(", ")
+  }
+
+  def kPrimes(k: Int, start: Long, end: Long): List[Int] = {
+    def loop(n: Int, acc: Stream[Int]): Stream[Int] ={
+      if(n > end) acc
+      else if(n > 1 && isKPrime(n, k)) loop(n + 1, acc :+ n)
+      else loop(n + 1, acc)
+    }
+
+    loop(start.toInt, Stream.empty).toList
   }
 
   private def isKPrime(candidate: Int, kfactors: Int, divisor: Int = 2): Boolean = (candidate, kfactors, divisor) match {
@@ -18,7 +31,16 @@ object KPrimes {
     case (_, _, _) => isKPrime(candidate, kfactors, divisor + 1)
   }
 
+  /*
+    128, 8 and 1 are respectively the smallest almost primes for 7-k, 3-k, 1-k
+   */
   def puzzle(s: Int): Int = {
-    ???
+    val factors = for {
+      sevens <- kPrimes(7, 128, s)
+      threes <- kPrimes(3, 8, s)
+      ones <- kPrimes(1, 1, s)
+      if sevens + threes + ones == s
+    } yield (sevens, threes, ones)
+    factors.size
   }
 }
--- src/test/scala/ca/vgorcinschi/KPrimesTest.scala
+++ src/test/scala/ca/vgorcinschi/KPrimesTest.scala
@@ -6,12 +6,21 @@ import org.scalatest.Assertions._
 import KPrimesTest._
 
 class KPrimesTest extends FlatSpec {
+
+  behavior of "countKprimes"
+
   it should "pass basic tests" in {
     testing(KPrimes.countKprimes(2, 0, 100),
       "4, 6, 9, 10, 14, 15, 21, 22, 25, 26, 33, 34, 35, 38, 39, 46, 49, 51, 55, 57, 58, 62, 65, 69, 74, 77, 82, 85, 86, 87, 91, 93, 94, 95")
     testing(KPrimes.countKprimes(12, 100000, 100100), "")
     testing(KPrimes.countKprimes(1, 2, 30), "2, 3, 5, 7, 11, 13, 17, 19, 23, 29")
     testing(KPrimes.countKprimes(8, 10000000, 10000200), "10000096, 10000152, 10000165, 10000200")
+  }
+
+  behavior of "puzzles"
+
+  it should "return # of solutions for puzzles" in {
+    assertResult(KPrimes.puzzle(138)){1}
   }
 }
 
